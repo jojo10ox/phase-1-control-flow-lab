cb9340cfd2aa3ec411c943908edfa0c3d297b131
Josephine Luu <josephinegluu@gmail.com>
2022-06-29 12:31:22 -0500

    commit tree 8a1465a468a5b0c9cb4c0aebc32e25f2a10ed861

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideHeight){
   // Write your code here!
+  if (rideHeight <= 400){
+    return 'This one is on me!';
+  }
+  else if (rideHeight <= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (2000 < rideHeight && rideHeight < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  if (cityName === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else{
+    return 'No go.';
+  }
+
+  //or return cityName === "NYC" ? "Ok, sounds good.":"No go";
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(typeOfTip){
   // Write your code here!
-}
+  switch (typeOfTip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return'Bye.';
+      break;
+      
+    }
+  }
